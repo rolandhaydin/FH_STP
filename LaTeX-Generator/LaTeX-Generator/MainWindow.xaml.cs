90a1a754da77162cb9febb6d5af602cd0bc37643
Roland <50742356+rolandhaydin@users.noreply.github.com>
2024-01-20 14:11:34 +0100

    Add LaTeX Generator

---
+++ LaTeX-Generator/LaTeX-Generator/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+namespace LaTeX_Generator
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PNG|*.png|JPG|*.jpg|JPEG|*.jpeg";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = @"C:\temp\blabla";
+            
+            if (ofd.ShowDialog() == true)
+            {
+                if(ofd.FileNames.Length != 0)
+                {
+                    generateFile(ofd.FileNames);
+                }
+            }
+            txtCaption.Text = "";
+            txtLatexPath.Text = "";
+            txtFigure.Text = "";
+        }
+        private void generateFile(string[] fn)
+        {
+            string latexpath = txtLatexPath.Text;
+            string caption = txtCaption.Text;
+            string figure = txtFigure.Text;
+            List<string> files = new List<string>();
+            int lastindex = fn[0].LastIndexOf('\\');
+            string outputpath = fn[0].Substring(0, lastindex);
+            string temp;
+            string temp2 = "";
+            int index = 1;
+            foreach(var filename in fn)
+            {
+                temp = filename.Split('\\')[filename.Split('\\').Length - 1];
+                files.Add(temp);
+            }
+            foreach(var filename in files)
+            {
+                temp = "\\begin figure[H]\n" +
+                       "    \\centering\n" +
+                       "    \\includegraphics[width=0.9\\linewidth]{" + latexpath + "/" + filename + "}\n" +
+                       "    \\caption{" + caption + " - " + index + "}\n" +
+                       "    \\label{fig:" + figure + "_" + index + "}\n" +
+                       "\\end{figure}\n";
+                index++;
+                temp2 += temp;
+            }
+            File.WriteAllText(outputpath + "\\output.txt", temp2);
+            Process.Start(outputpath);
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
